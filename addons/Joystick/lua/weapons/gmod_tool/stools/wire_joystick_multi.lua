b86e42dda1adc698ccee79055bc641b0e705908f
Deyan Dobromirov <dvd_video@abv.bg>
2020-10-04 23:47:03 +0300

    Update wire_joystick_multi.lua

--- addons/Joystick/lua/weapons/gmod_tool/stools/wire_joystick_multi.lua
+++ addons/Joystick/lua/weapons/gmod_tool/stools/wire_joystick_multi.lua
@@ -103,10 +103,12 @@ end
 
 function TOOL:GetControlUID(sIdx, bVal)
   local out = SanitizeUID(self:GetClientInfo(sIdx.."uid"))
-  local ok, err = jcon.isValidUID(out)
-  if ( bVal and not ok ) then out = nil
-    ErrorNoHalt("Wire Joystick: "..tostring(err).."\n")
-  end -- Validate the UID when requested
+  if ( bVal ) then -- Force validation of UID
+    local ok, err = jcon.isValidUID(out)
+    if ( not ok ) then out = nil
+      ErrorNoHalt("Wire Joystick: "..tostring(err).."\n")
+    end -- Validate the UID when requested
+  end
   return out
 end
 
